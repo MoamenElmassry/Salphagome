72856aef915e249594ca55f4e50b60cca831e5fa
Moamen Elmassry <moamen.elmassry@gmail.com>
2020-05-07 18:37:00 -0500

    Update Script pipeline.R

--- Script pipeline.R
+++ Script pipeline.R
@@ -1,4 +1,6 @@
 
-  <i>bash magicblast -db $BLAST_DB -sra $i -no_unaligned -num_threads $THREADS -out $OUT_DIR/$i.sam</i>
-
+#Run Magic-BLAST
+```
+magicblast -db $BLAST_DB -sra $i -no_unaligned -num_threads $THREADS -out $OUT_DIR/$i.sam
+```
 
